56a5adb8c156008e1cd38761da61633d24a10eec
stianlars1 <stian.larsen@mac.com>
2026-01-03 22:48:11 +0100

    wtf

--- src/components/BorderBeam/index.ts
+++
@@ -1,2 +0,0 @@
-export { BorderBeam } from "./BorderBeam";
-export type { BorderBeamProps } from "./BorderBeam.types";
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-export { BorderBeam } from "./components/BorderBeam";
-export type { BorderBeamProps } from "./components/BorderBeam";
